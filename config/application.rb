3a4248d1ba7c024483f25cf52f85101ae2f2938e
misha434 <missymouse434@gmail.com>
2021-05-30 19:18:54 +0900

    Add RSpec attachment files(chenge config)
    RSpec関連のファイルを移行(設定変更)

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,6 @@ module Smar003
     config.generators.system_tests = nil
     config.generators do |g|
       g.test_framework  :rspec,
-        fixtures: false,
         view_specs: false,
         helper_specs: false,
         routing_specs: false
