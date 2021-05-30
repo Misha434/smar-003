9c1e34a25c26817f18af3fd2c7bbf11e57e6a533
Miisha434 <missymouse434@gmail.com>
2021-05-30 15:27:23 +0900

    Setting for webpacker
    webpacker セッティングのため

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -2,6 +2,9 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+// frontend/packs/application.js
+import "./application.css";
+document.body.insertAdjacentHTML("afterbegin", "Webpacker works!");
 
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
@@ -12,5 +15,5 @@ require("channels")
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
