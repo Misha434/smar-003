f474cabb9f7a7234475a9fecaafceba98838f987
misha434 <missymouse434@gmail.com>
2021-05-31 20:07:15 +0900

    Add users#index users#show
    ユーザー一覧・詳細画面の設定

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  before_action :admin_user, only: :index
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    # @review = Review.new
+    # @reviews = @user.reviews
+    # @brands = Brand.all
+    # @products = Product.all
+    # @user_received_like_countup = Like.joins(review: :user).where('users.id=?', params[:id]).count
+  end
+end
