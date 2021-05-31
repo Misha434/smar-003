04d965cb7c3e68828700d69a2686af0d91b7851e
misha434 <missymouse434@gmail.com>
2021-05-31 19:03:19 +0900

    Set params for adding :name
    User model に ユーザー名追加のための設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   prepend_view_path Rails.root.join("frontend")
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+    def admin_user
+      @user = authenticate_user!
+      redirect_to @user || request.referrer unless @user.admin?
+    end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
 end
