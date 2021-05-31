43375d5b4d598fe0d7e03ce512e090e8977a65e3
misha434 <missymouse434@gmail.com>
2021-05-31 19:13:33 +0900

    Add params for user avatar
    プロフ画像のための設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,8 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:account_update, keys: %i(avatar))
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i(avatar))
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     end
