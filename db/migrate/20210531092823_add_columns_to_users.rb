69b068f5619ac4ee2c76421746bef17eb9eed1b7
misha434 <missymouse434@gmail.com>
2021-05-31 20:11:27 +0900

    Add default setting for admin
    admin カラムに初期設定追加

--- db/migrate/20210531092823_add_columns_to_users.rb
+++ db/migrate/20210531092823_add_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :name, :string
-    add_column :users, :admin, :boolean
+    add_column :users, :admin, :boolean, default: false
   end
 end
