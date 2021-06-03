1c5086f242fde457eb0024c4a2047752e9c4d646
misha434 <missymouse434@gmail.com>
2021-06-03 21:52:27 +0900

    Add Review CRUD
    レビュー基本機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show]
 	root 'pages#home'
+	resources :reviews, only: [:create, :destroy, :new, :edit, :update]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
 3.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@example.org"
@@ -18,4 +17,18 @@
     password_confirmation: password,
     avatar: avatar 
     )
+end
+
+Review.create!(
+  content: "Good",
+  user_id: 1
+  )
+
+10.times do
+  content = Faker::Lorem.sentence(word_count: 10)
+  user_id = Faker::Number.within(range: 1..3)
+  Review.create!(
+    content: content,
+    user_id: user_id,
+  )
 end
