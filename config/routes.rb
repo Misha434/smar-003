4f1c69700a287923f86fd6aa83320f41bd4503cc
misha434 <missymouse434@gmail.com>
2021-06-04 10:33:56 +0900

    Add routing for Brand
    ブランドのルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,18 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', users: 'users' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show]
+  # get '/contact', to: 'pages#contact'
+  # get '/terms', to: 'pages#terms'
 	root 'pages#home'
-	resources :reviews, only: [:create, :destroy, :new, :edit, :update]
+  resources :reviews, only: [:create, :destroy, :new, :edit, :update]
+	# resources :products
+	# resources :products do
+	#   post :import, on: :collection
+	# end
+	resources :brands 
+	# resources :brands do
+	#   post :import, on: :collection
+	# end
+  # post 'like/:id' => 'likes#create', as: 'create_like'
+  # delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
 end
