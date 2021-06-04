36c56d44d8dd04046ca60df8491bbe378745301c
misha434 <missymouse434@gmail.com>
2021-06-04 15:06:49 +0900

    Add foreign_key columns in all models
    モデル内 FK関連のカラム追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,12 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :user
-  # belongs_to :product
+  belongs_to :product
   has_one_attached :image
   # default_scope -> { order(created_at: :desc)}
   # has_many :likes, dependent: :destroy
   
   validates :user_id, presence: true
-  # validates :product_id, presence: true
+  validates :product_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validates :image, content_type: { in: %w[image/jpeg image/gif image/png], \
     message: "must be a valid image format" }, \
