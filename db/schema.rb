98998bae18514bc0ca85609ac3bcb358c0b8d357
misha434 <missymouse434@gmail.com>
2021-06-04 15:06:49 +0900

    Add foreign_key columns in all models
    モデル内 FK関連のカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_024652) do
+ActiveRecord::Schema.define(version: 2021_06_04_054957) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 2021_06_04_024652) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "product_id", null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
@@ -73,5 +75,6 @@ ActiveRecord::Schema.define(version: 2021_06_04_024652) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "products", "brands"
+  add_foreign_key "reviews", "products"
   add_foreign_key "reviews", "users"
 end
