8359ad738cc169ac13a14fc9ca5da0a047ed993e
misha434 <missymouse434@gmail.com>
2021-06-04 16:33:09 +0900

    Add dynamic titles
    動的タイトルを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,12 +9,12 @@ module ApplicationHelper
     end
   end
   
-  # def full_title(page_title = '')
-  #   base_title = "SmaR"
-  #   if page_title.empty?
-  #     base_title
-  #   else
-  #     page_title + " - " + base_title
-  #   end
-  # end
+  def full_title(page_title = '')
+    base_title = "SmaR"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " - " + base_title
+    end
+  end
 end
