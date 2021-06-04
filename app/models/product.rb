a29abd4d5ecc830aa3f380c5eaf7b9dec32fcfeb
misha434 <missymouse434@gmail.com>
2021-06-04 18:28:41 +0900

    Rmove '\' in models
    バックスラッシュ削除

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,9 +6,9 @@ class Product < ApplicationRecord
   validates :name, presence: true, length: {in: 1..50}
   validates :soc_antutu_score, presence: true
   validates :battery_capacity, presence: true
-  validates :image, content_type: { in: %w[image/jpeg image/gif image/png], \
-    message: "must be a valid image format" }, \
-    size: { less_than: 5.megabytes, \
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
+    message: "must be a valid image format" },
+    size: { less_than: 5.megabytes,
     message: "should be less than 5MB" }
   
   # def self.csv_attributes
