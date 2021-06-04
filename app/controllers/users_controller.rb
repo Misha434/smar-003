fb7ac34abf3406e87352eab8c3aaa434b73a6ba0
misha434 <missymouse434@gmail.com>
2021-06-04 17:35:35 +0900

    Remove comment out to indicate reviews in users#show
    ユーザー詳細画面に投稿内容表示のためコメントアウト解除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @review = Review.new
-    # @reviews = @user.reviews
+    @reviews = @user.reviews
     @brands = Brand.all
     @products = Product.all
     # @user_received_like_countup = Like.joins(review: :user).where('users.id=?', params[:id]).count
