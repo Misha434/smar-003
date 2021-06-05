d3c1f310b7fdb4f8c6c2bff1584f6e75b1ffe6d4
misha434 <missymouse434@gmail.com>
2021-06-05 20:50:24 +0900

    Fix validates for content length in review
    レビューの字数制限設定のため

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,7 +7,7 @@ class Review < ApplicationRecord
   
   validates :user_id, presence: true
   validates :product_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { in: 1..140 }
   validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
     message: "must be a valid image format" },
     size: { less_than: 5.megabytes,
