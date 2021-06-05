1a19bbaf632b800ae1253738c7708597f6236591
misha434 <missymouse434@gmail.com>
2021-06-05 20:56:10 +0900

    Attach image in users#create
    ユーザー作成時 画像登録対応

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+    @user.avatar.attach(params[:user][:avatar])
+  end
 
   # GET /resource/edit
   # def edit
