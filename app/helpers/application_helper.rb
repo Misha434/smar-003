2f585b0060a61b4e020f284998dc1d9b32fc0637
misha434 <missymouse434@gmail.com>
2021-06-12 14:43:02 +0900

    Fix image_tag in default_product_image
    画像未登録の製品画像のパスを変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def product_image_default(product)
     if product.image.attached?
-      link_to image_tag(product.image, width: "75px",
+      link_to image_pack_tag(product.image, width: "75px",
       class: "img-fluid"), product_path(product)
     else
-      link_to image_tag("product_no_image.jpeg",
+      link_to image_pack_tag("product_no_image.jpeg",
       width: "75px", class: "img-fluid"), product_path(product)
     end
   end
