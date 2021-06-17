dd3f46467bfc12985bc4c962c639ddefdb4057b8
misha434 <missymouse434@gmail.com>
2021-06-17 17:21:32 +0900

    Add ajax like button(products#show)
    製品詳細ページ 非同期通信いいねボタンに修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,19 +2,11 @@ class LikesController < ApplicationController
   before_action :review_params, only: [:create, :destroy]
   
   def create
-    @like = Like.new(user_id: current_user.id, review_id: params[:id])
-    if @like.save
-      flash[:notice] = "Like is success"
-      redirect_to request.referrer
-    else
-      flash[:alert] = "Like is fail"
-      redirect_to request.referrer
-    end
+    Like.create(user_id: current_user.id, review_id: params[:id])
   end
   
   def destroy
     Like.find_by(user_id: current_user.id, review_id: params[:id]).destroy
-    redirect_to request.referrer
   end
 
   private
