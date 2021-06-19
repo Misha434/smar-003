dde441177b0e95a660a1b80f90b8896a78bd65a8
misha434 <missymouse434@gmail.com>
2021-06-19 18:52:24 +0900

    [RSpec] Modify spec/models/product.rb, Product form
    製品フォーム バリデーションチェック 修正

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,81 +3,448 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   before do
     @brand = FactoryBot.create(:brand)
-  end
-
-  # 有効なファクトリを持つこと
-  it "has a valid factory" do
-    expect(FactoryBot.build(:product, brand_id: @brand.id)).to be_valid
-  end
-
-  it { should validate_presence_of(:name) }
-
-  it { should validate_presence_of(:soc_antutu_score) }
-
-  it { should validate_presence_of(:battery_capacity) }
-
-  it { should validate_presence_of(:brand_id) }
-
-  # 製品画像 アップロードが有効であること
-  it "is valid with a real image data" do
     @product = FactoryBot.build(:product)
-    @product.image = fixture_file_upload("files/image/image_test_logo.png")
-    expect(@product.valid?).to eq true
   end
-
-  # gif 製品画像 アップロードが有効であること
-  it "is valid with a GIF image data" do
-    @product = FactoryBot.build(:product)
-    @product.image = fixture_file_upload("files/image/image_test_3kb.gif")
-    expect(@product.valid?).to eq true
+  describe 'Name Form filled-out' do
+    it 'is a valid' do
+      expect(@product).to be_valid
+    end
+    it 'with an attached image is a valid' do
+      @product.image = fixture_file_upload("files/image/image_test_logo.png")
+      expect(@product).to be_valid
+    end
   end
-
-  # jpeg 製品画像 アップロードが有効であること
-  it "is valid with a JPEG image data" do
-    @product = FactoryBot.build(:product)
-    @product.image = fixture_file_upload("files/image/image_test_3kb.jpeg")
-    expect(@product.valid?).to eq true
+  describe "Name Form" do
+    describe "filled with word length" do
+      it { should validate_presence_of(:name) }      
+      it do
+        should validate_length_of(:name).
+        is_at_least(1).is_at_most(50)
+      end
+      it { should_not validate_length_of(:name).is_at_most(51) }
+    end
+    describe "Charactor Type" do
+      context "漢字・ひらがな・カタカナ(全角)" do
+        it "is valid" do
+          product = FactoryBot.build(:product, name: "吾輩は猫である。名前はまだ無い。どこで生れたか見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー")
+          expect(product).to be_valid
+        end
+      end
+      context "半角カタカナ" do
+        it "is valid" do
+          product = FactoryBot.build(:product, name: "ﾜｶﾞﾊｲﾊﾈｺﾃﾞｱﾙ｡ﾅﾏｴﾊﾏﾀﾞﾅｲ｡ﾄﾞｺﾃﾞｳﾏﾚﾀｶｹﾝﾄｳｶﾞﾂｶﾇ｡ﾅﾝﾃﾞﾓｳｽ")
+          expect(product).to be_valid
+        end
+      end
+      context "English(Upper/Down Case)" do
+        it "is valid" do
+          product = FactoryBot.build(:product, name: "From fairest creatures we desire increase, That th")
+          expect(product).to be_valid
+        end
+      end
+      context "symbol" do
+        it "is valid" do
+          product = FactoryBot.build(:product, name: "▼※〒→←↑↓∇∵Å‰†‡ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμν")
+          expect(product).to be_valid
+        end
+      end
+      context "Number" do
+        it "is valid" do
+          product = FactoryBot.build(:product, name: "88991646493833403４５３１７５１９０２４８７５１０４３６５１８２７４６１８２5583")
+          expect(product).to be_valid
+        end
+      end
+      context "Emoji" do
+        it "is valid" do
+          product = FactoryBot.build(:product, name: "👨‍👩‍👦‍👦"*50)
+          expect(product).to_not be_valid
+        end
+        it "is invalid 51 charactors" do
+          product = FactoryBot.build(:product, name: "👨‍👩‍👦‍👦"*51)
+          expect(product).to_not be_valid
+        end
+      end
+    end
+    describe "Registrated Name" do
+      before do
+        @product.save!
+      end
+      describe 'in same Upper/Down cases' do
+        it "is invalid" do
+          same_name_product = FactoryBot.build(:product)
+          expect(same_name_product).to_not be_valid
+        end
+      end
+      describe 'in same Upper cases' do
+        it "is invalid" do
+          same_name_product = FactoryBot.build(:product, name: "PHONE-1")
+          expect(same_name_product).to_not be_valid
+        end
+      end
+      describe 'in same down cases' do
+        it "is invalid" do
+          same_name_product = FactoryBot.build(:product, name: "phone-1")
+          expect(same_name_product).to_not be_valid
+        end
+      end
+      describe 'in Japanese,' do
+        it "ひらがな and 全角カタカナ is valid" do
+          same_name_product_hiragana = FactoryBot.build(:product, name: "ふぉん-1")
+          same_name_product_full_width_katakana = FactoryBot.build(:product, name: "フォン-1")
+          expect(same_name_product_hiragana).to be_valid
+          expect(same_name_product_full_width_katakana).to be_valid
+        end
+        it "半角カタカナ and 全角カタカナ is valid" do
+          same_name_product_half_width_katakana = FactoryBot.build(:product, name: "ﾌｫﾝ-1")
+          same_name_product_full_width_katakana = FactoryBot.build(:product, name: "フォン-1")
+          expect(same_name_product_half_width_katakana).to be_valid
+          expect(same_name_product_full_width_katakana).to be_valid
+        end
+      end
+    end
   end
-
-  # png 製品画像 アップロードが有効であること
-  it "is valid with a PNG image data" do
-    @product = FactoryBot.build(:product)
-    @product.image = fixture_file_upload("files/image/image_test_3kb.png")
-    expect(@product.valid?).to eq true
+  describe "Brand Form" do
+    describe "filled with word length" do
+      it "-1 is invalid(int)" do
+        product = FactoryBot.build(:product, brand_id: -1)
+        expect(product).to_not be_valid
+      end
+      it "0 is invalid(int)" do
+        product = FactoryBot.build(:product, brand_id: 0)
+        expect(product).to_not be_valid
+      end
+      it { should validate_presence_of(:brand_id) }
+      it "1 is valid(int)" do
+        product = FactoryBot.build(:product, brand_id: 1)
+        expect(product).to be_valid
+      end
+      it "-1 is invalid(string)" do
+        product = FactoryBot.build(:product, brand_id: "-1")
+        expect(product).to_not be_valid
+      end
+      it "0 is invalid(string)" do
+        product = FactoryBot.build(:product, brand_id: "0")
+        expect(product).to_not be_valid
+      end
+      it "1 is valid(string)" do
+        product = FactoryBot.build(:product, brand_id: "1")
+        expect(product).to be_valid
+      end
+    end
+    describe "Charactor Type" do
+      context "漢字・ひらがな・カタカナ(全角)" do
+        it "is invalid" do
+          product = FactoryBot.build(:product, brand_id: "後")
+          expect(product).to_not be_valid
+        end
+      end
+      context "半角カタカナ" do
+        it "is invalid" do
+          product = FactoryBot.build(:product, brand_id: "ﾜｶﾞ")
+          expect(product).to_not be_valid
+        end
+      end
+      context "English(Upper/Down Case)" do
+        it "is invalid" do
+          product = FactoryBot.build(:product, brand_id: "Fr")
+          expect(product).to_not be_valid
+        end
+      end
+      context "symbol" do
+        it "is invalid" do
+          product = FactoryBot.build(:product, brand_id: "▼")
+          expect(product).to_not be_valid
+        end
+      end
+      context "Number" do
+        it "is valid" do
+          product = FactoryBot.build(:product, brand_id: "1")
+          expect(product).to be_valid
+        end
+      end
+      context "Emoji" do
+        it "is valid" do
+          product = FactoryBot.build(:product, brand_id: "👨‍👩‍👦‍👦")
+          expect(product).to be_valid
+        end
+      end
+    end
+    describe "Registrated brand_id" do
+      before do
+        @product.save!
+      end
+      describe 'in same Upper cases' do
+        it "is invalid" do
+          same_name_product = FactoryBot.build(:product, brand_id: "１")
+          expect(same_name_product).to_not be_valid
+        end
+      end
+    end
   end
-
-  # svg 製品画像 アップロードが無効であること
-  it "is invalid with a SVG image data" do
-    @product = FactoryBot.build(:product)
-    @product.image = fixture_file_upload("files/image/image_test_3kb.svg")
-    expect(@product.valid?).to eq false
+  describe "Soc Antutu Score" do
+    describe "under data-type(string)" do
+      describe "Charactor Type" do
+        context "漢字・ひらがな・カタカナ(全角)" do
+          it "is invalid" do
+            product = FactoryBot.build(:product, soc_antutu_score: "後あア")
+            expect(product).to_not be_valid
+          end
+        end
+        context "半角カタカナ" do
+          it "is invalid" do
+            product = FactoryBot.build(:product, soc_antutu_score: "ﾜｶﾞ")
+            expect(product).to_not be_valid
+          end
+        end
+        context "English(Upper/Down Case)" do
+          it "is invalid" do
+            product = FactoryBot.build(:product, soc_antutu_score: "Fr")
+            expect(product).to_not be_valid
+          end
+        end
+        context "symbol" do
+          it "is invalid" do
+            product = FactoryBot.build(:product, soc_antutu_score: "▼")
+            expect(product).to_not be_valid
+          end
+        end
+        context "Number" do
+          it "(harf-width) is valid" do
+            product = FactoryBot.build(:product, soc_antutu_score: "1")
+            expect(product).to be_valid
+          end
+          it "(full-width) is valid" do
+            product = FactoryBot.build(:product, soc_antutu_score: "１")
+            expect(product).to be_valid
+          end
+        end
+        context "Emoji" do
+          it "is valid" do
+            product = FactoryBot.build(:product, soc_antutu_score: "👨‍👩‍👦‍👦")
+            expect(product).to be_valid
+          end
+        end
+      end
+    end
+    describe "data-type(integer)" do
+      context 'harf-width' do
+        it "-1 is valid" do
+          product = FactoryBot.build(:product, soc_antutu_score: -1)
+          expect(product).to_not be_valid
+        end
+        it "0(zero) is valid" do
+          product = FactoryBot.build(:product, soc_antutu_score: 0)
+          expect(product).to_not be_valid
+        end
+        it "1 is valid" do
+          product = FactoryBot.build(:product, soc_antutu_score: 1)
+          expect(product).to be_valid
+        end
+      end
+      context 'full-width' do
+        it "-１ is invalid" do
+          product = FactoryBot.build(:product, soc_antutu_score: -１)
+          expect(product).to_not be_valid
+        end
+        it "０ is invalid" do
+          product = FactoryBot.build(:product, soc_antutu_score: ０)
+          expect(product).to_not be_valid
+        end
+        it "１ is invalid" do
+          product = FactoryBot.build(:product, soc_antutu_score: １)
+          expect(product).to_not be_valid
+        end
+      end
+    end
+    describe "data-type(float)" do
+      context 'harf-width' do
+        it "-1.1 is invalid" do
+          product = FactoryBot.build(:product, soc_antutu_score: -1.1)
+          expect(product).to_not be_valid
+        end
+        it "-1.0 is invalid" do
+          product = FactoryBot.build(:product, soc_antutu_score: -1.0)
+          expect(product).to_not be_valid
+        end
+        it "-0.1 is invalid" do
+          product = FactoryBot.build(:product, soc_antutu_score: -0.1)
+          expect(product).to_not be_valid
+        end
+        it "0.0(zero) is invalid" do
+          product = FactoryBot.build(:product, soc_antutu_score: 0.0)
+          expect(product).to_not be_valid
+        end
+        it "0.1 is invalid" do
+          product = FactoryBot.build(:product, soc_antutu_score: 0.1)
+          expect(product).to_not be_valid
+        end
+        it "1.0 is invalid" do
+          product = FactoryBot.build(:product, soc_antutu_score: 1.0)
+          expect(product).to_not be_valid
+        end
+        it "1.1 is invalid" do
+          product = FactoryBot.build(:product, soc_antutu_score: 1.0)
+          expect(product).to_not be_valid
+        end
+      end
+    end
   end
-
-  # psd 製品画像 アップロードが無効であること
-  it "is valid with a PSD image data" do
-    @product = FactoryBot.build(:product)
-    @product.image = fixture_file_upload("files/image/image_test_3kb.psd")
-    expect(@product.valid?).to eq false
+  describe "Battery Capacity" do
+    describe "under data-type(string)" do
+      describe "Charactor Type" do
+        context "漢字・ひらがな・カタカナ(全角)" do
+          it "is invalid" do
+            product = FactoryBot.build(:product, battery_capacity: "後あア")
+            expect(product).to_not be_valid
+          end
+        end
+        context "半角カタカナ" do
+          it "is invalid" do
+            product = FactoryBot.build(:product, battery_capacity: "ﾜｶﾞ")
+            expect(product).to_not be_valid
+          end
+        end
+        context "English(Upper/Down Case)" do
+          it "is invalid" do
+            product = FactoryBot.build(:product, battery_capacity: "Fr")
+            expect(product).to_not be_valid
+          end
+        end
+        context "symbol" do
+          it "is invalid" do
+            product = FactoryBot.build(:product, battery_capacity: "▼")
+            expect(product).to_not be_valid
+          end
+        end
+        context "Number" do
+          it "(harf-width) is valid" do
+            product = FactoryBot.build(:product, battery_capacity: "1")
+            expect(product).to be_valid
+          end
+          it "(full-width) is valid" do
+            product = FactoryBot.build(:product, battery_capacity: "１")
+            expect(product).to be_valid
+          end
+        end
+        context "Emoji" do
+          it "is valid" do
+            product = FactoryBot.build(:product, battery_capacity: "👨‍👩‍👦‍👦")
+            expect(product).to be_valid
+          end
+        end
+      end
+    end
+    describe "data-type(integer)" do
+      context 'harf-width' do
+        it "-1 is valid" do
+          product = FactoryBot.build(:product, battery_capacity: -1)
+          expect(product).to_not be_valid
+        end
+        it "0(zero) is valid" do
+          product = FactoryBot.build(:product, battery_capacity: 0)
+          expect(product).to_not be_valid
+        end
+        it "1 is valid" do
+          product = FactoryBot.build(:product, battery_capacity: 1)
+          expect(product).to be_valid
+        end
+      end
+      context 'full-width' do
+        it "-１ is invalid" do
+          product = FactoryBot.build(:product, battery_capacity: -１)
+          expect(product).to_not be_valid
+        end
+        it "０ is invalid" do
+          product = FactoryBot.build(:product, battery_capacity: ０)
+          expect(product).to_not be_valid
+        end
+        it "１ is invalid" do
+          product = FactoryBot.build(:product, battery_capacity: １)
+          expect(product).to_not be_valid
+        end
+      end
+    end
+    describe "data-type(float)" do
+      context 'harf-width' do
+        it "-1.1 is invalid" do
+          product = FactoryBot.build(:product, battery_capacity: -1.1)
+          expect(product).to_not be_valid
+        end
+        it "-1.0 is invalid" do
+          product = FactoryBot.build(:product, battery_capacity: -1.0)
+          expect(product).to_not be_valid
+        end
+        it "-0.1 is invalid" do
+          product = FactoryBot.build(:product, battery_capacity: -0.1)
+          expect(product).to_not be_valid
+        end
+        it "0.0(zero) is invalid" do
+          product = FactoryBot.build(:product, battery_capacity: 0.0)
+          expect(product).to_not be_valid
+        end
+        it "0.1 is invalid" do
+          product = FactoryBot.build(:product, battery_capacity: 0.1)
+          expect(product).to_not be_valid
+        end
+        it "1.0 is invalid" do
+          product = FactoryBot.build(:product, battery_capacity: 1.0)
+          expect(product).to_not be_valid
+        end
+        it "1.1 is invalid" do
+          product = FactoryBot.build(:product, battery_capacity: 1.0)
+          expect(product).to_not be_valid
+        end
+      end
+    end
   end
-
-  # bmp 製品画像 アップロードが無効であること
-  it "is valid with a BMP image data" do
-    @product = FactoryBot.build(:product)
-    @product.image = fixture_file_upload("files/image/image_test_3kb.bmp")
-    expect(@product.valid?).to eq false
-  end
-
-  # 5MBの製品画像 アップロードは有効であること
-  it "is valid with a image data 5MB" do
-    @product = FactoryBot.build(:product)
-    @product.image = fixture_file_upload("files/image/image_test_5mb.jpeg")
-    expect(@product.valid?).to eq true
-  end
-
-  # 6MB以上の製品画像 アップロードは無効であること
-  it "is invalid with a image data over 6MB" do
-    brand = FactoryBot.build(:product)
-    brand.image = fixture_file_upload("files/image/image_test_6mb.jpeg")
-    expect(brand.valid?).to eq false
+  describe "image" do
+    describe "File" do
+      context "has a GIF format" do
+        it "is valid" do
+          @product.image = fixture_file_upload("files/image/image_test_3kb.gif")
+          expect(@product).to be_valid
+        end
+      end
+      context "has a jpeg format" do
+        it "is valid" do
+          @product.image = fixture_file_upload("files/image/image_test_3kb.jpeg")
+          expect(@product).to be_valid
+        end
+      end
+      context "has a PNG format" do
+        it "is valid" do
+          @product.image = fixture_file_upload("files/image/image_test_3kb.png")
+          expect(@product).to be_valid
+        end
+      end
+      context "has a SVG format" do
+        it "is invalid" do
+          @product.image = fixture_file_upload("files/image/image_test_3kb.svg")
+          expect(@product).to_not be_valid
+        end
+      end
+      context "has a PSD format" do
+        it "is invalid" do
+          @product.image = fixture_file_upload("files/image/image_test_3kb.psd")
+          expect(@product).to_not be_valid
+        end
+      end
+      context "has a BMP format" do
+        it "is invalid" do
+          @product.image = fixture_file_upload("files/image/image_test_3kb.bmp")
+          expect(@product).to_not be_valid
+        end
+      end
+    end
+    describe "File size" do
+      it "5MB is valid" do
+        @product.image = fixture_file_upload("files/image/image_test_5mb.jpeg")
+        expect(@product).to be_valid
+      end
+      it "6MB is invalid" do
+        @product.image = fixture_file_upload("files/image/image_test_6mb.jpeg")
+        expect(@product).to_not be_valid
+      end
+    end
   end
 end
