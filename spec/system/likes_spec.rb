b763e48c8f98681971852750daf220d4e6dd7207
misha434 <missymouse434@gmail.com>
2021-06-23 20:20:18 +0900

    [RSpec system] Modify likes_spec for js: true
    いいね機能 Ajax化に伴う テストの js 有効化

--- spec/system/likes_spec.rb
+++ spec/system/likes_spec.rb
@@ -11,36 +11,32 @@ RSpec.describe Like, type: :system do
       fill_in "Email", with: @user.email
       fill_in "Password", with: @user.password
       click_button "Log in"
+      visit '/products/1'
     end
     describe 'Product Page' do
-      before do
-        visit '/products/1'
-      end
-      describe 'products/1' do
+      describe 'products/1', js: true do
         it 'has an available good button' do
           expect(page).to have_content("Phone-1")
-          within first(:css, 'span.each-like-countup') do
+          within 'span.each-like-countup' do
             expect(page).to have_content("0")
           end
           within '#review-1' do
             click_link '0'
           end
-          within first(:css, 'span.each-like-countup') do
+          within 'span.each-like-countup' do
             expect(page).to have_content("1")
           end
         end
-
-        it 'has an available good button for delete good' do
+      end
+      describe 'reset good button', js: true do
+        it 'is available' do
           within '#review-1' do
             click_link '0'
-          end
-          within first(:css, 'span.each-like-countup') do
-            expect(page).to have_content("1")
           end
           within '#review-1' do
             click_link '1'
           end
-          within first(:css, 'span.each-like-countup') do
+          within 'span.each-like-countup' do
             expect(page).to have_content("0")
           end
         end
