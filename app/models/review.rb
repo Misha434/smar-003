545f41c1681f34cec0a8a5134b7065f03ec6f3cb
misha434 <missymouse434@gmail.com>
2021-07-05 15:24:30 +0900

    [Model review] Add validates only integer and value range
    rating のための制約追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -10,9 +10,13 @@ class Review < ApplicationRecord
   validates :product_id, presence: true,
                          numericality: { only_integer: true,
                                          greater_than_or_equal_to: 1 }
+  validates :rate, presence: true,
+                   numericality: { only_integer: true,
+                                   greater_than_or_equal_to: 1,
+                                   less_than_or_equal_to: 5 }
   validates :content, presence: true, length: { in: 1..140 }
   validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
-                                    message: "must be a valid image format" },
+                                    message: "must be valid image format" },
                     size: { less_than: 5.megabytes,
                             message: "should be less than 5MB" }
 end
