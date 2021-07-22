9ee21c53e52dad56a76977e8fc5665849b5e6911
misha434 <missymouse434@gmail.com>
2021-07-22 13:33:19 +0900

    [RSpec] Add Test case for Name form
    名前記入欄 テスト追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe User, type: :system do
       @brand = FactoryBot.create(:brand)
       @admin_user = FactoryBot.create(:user, name: "admin", admin: true)
       @general_user = FactoryBot.create(:user, id: 2, name: "general", email: 'test-1@example.com')
+      @general_user2 = FactoryBot.build(:user, id: 3, name: "Michael Smith", email: 'michael-m@example.com')
       @product_1 = FactoryBot.create(:product, id: 1, name: "Phone-1")
       @product_2 = FactoryBot.create(:product, id: 2, name: "Phone-2")
       @product_3 = FactoryBot.create(:product, id: 3, name: "Phone-3")
@@ -73,6 +74,194 @@ RSpec.describe User, type: :system do
           it 'is available' do
             click_button "Sign up"
             expect(page).to have_content 'Welcome'
+          end
+          it '(with avatar image)is available' do
+            attach_file "user_avatar",
+            "#{Rails.root}/spec/fixtures/files/image/image_test_5mb.jpeg"
+            click_button "Sign up"
+            expect(page).to have_content 'Welcome'
+          end
+        end
+        describe 'with Name form' do
+          describe 'of charactor count' do
+            context 'is 0(zero)' do
+              it 'is unavailable' do
+                fill_in 'Name', with: ''
+                click_button "Sign up"
+                expect(page).to have_content 'Sign up'
+                expect(page).to have_content "Name can't be blank"
+              end
+            end
+            context 'is 1' do
+              it 'is available' do
+                fill_in 'Name', with: 'X'
+                click_button "Sign up"
+                expect(page).to have_content 'Welcome'
+              end
+            end
+            context 'is 50' do
+              it 'is available' do
+                testdata_name = 'Aaron and associates Example Company East Asia Inc'
+                fill_in 'Name', with: testdata_name
+                click_button "Sign up"
+                expect(page).to have_content 'Welcome'
+              end
+            end
+            context 'is 51' do
+              it 'is unavailable' do
+                testdata_name = 'Philip and associates Example Company East Asia Inc'
+                fill_in 'Name', with: testdata_name
+                click_button "Sign up"
+                expect(page).to have_content 'Sign up'
+                expect(page).to have_content "Name is too long"
+              end
+            end
+          end
+          describe 'charactor type' do
+            context 'is 漢字・ひらがな・全角カタカナ' do
+              it 'is available' do
+                testdata_name = '株式会社東アジア・フィリップ・スミス・アンド・すずきたろう・アンド・さとうじろう・アソシエイツインク'
+                fill_in 'Name', with: testdata_name
+                click_button "Sign up"
+                expect(page).to have_content 'Welcome'
+              end
+            end
+            context 'is 半角カタカナ' do
+              it 'is available' do
+                testdata_name = 'ﾜｶﾞﾊｲﾊﾈｺﾃﾞｱﾙ｡ﾅﾏｴﾊﾏﾀﾞﾅｲ｡ﾄﾞｺﾃﾞｳﾏﾚﾀｶｹﾝﾄｳｶﾞﾂｶﾇ｡ﾅﾝﾃﾞﾓｳｽ'
+                fill_in 'Name', with: testdata_name
+                click_button "Sign up"
+                expect(page).to have_content 'Welcome'
+              end
+            end
+          end
+          context "English(Upper/Down Case)" do
+            it "is available" do
+              testdata_name = "From fairest creatures we desire increase, That th"
+              fill_in 'Name', with: testdata_name
+              click_button "Sign up"
+              expect(page).to have_content 'Welcome'
+            end
+          end
+          context "symbol" do
+            it "is available" do
+              testdata_name = "▼※〒→←↑↓∇∵Å‰†‡ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμν"
+              fill_in 'Name', with: testdata_name
+              click_button "Sign up"
+              expect(page).to have_content 'Welcome'
+            end
+          end
+          context "Number" do
+            it "is available" do
+              testdata_name = "88991646493833403４５３１７５１９０２４８７５１０４３６５１８２７４６１８２5583"
+              fill_in 'Name', with: testdata_name
+              click_button "Sign up"
+              expect(page).to have_content 'Welcome'
+            end
+          end
+          context "Emoji" do
+            it "is available" do
+              testdata_name = "👨" * 50
+              fill_in 'Name', with: testdata_name
+              click_button "Sign up"
+              expect(page).to have_content 'Welcome'
+            end
+            it "is unavailable 51 charactors" do
+              testdata_name = "👨" * 51
+              fill_in 'Name', with: testdata_name
+              click_button "Sign up"
+              expect(page).to have_content 'Welcome'
+            end
+          end
+          context "space" do
+            it "only is unavailable" do
+              fill_in 'Name', with: ' 　'
+              click_button "Sign up"
+              expect(page).to have_content 'Sign up'
+            end
+          end
+          describe 'registrated' do
+            it 'is available' do
+              fill_in 'Name', with: @general_user.name
+              fill_in 'Email', with: 'test-2@example.com'
+              click_button "Sign up"
+              expect(page).to have_content 'Welcome'
+            end
+          end
+          describe 'about image field' do
+            describe 'file format' do
+              context 'gif' do
+                it 'is available' do
+                  attach_file "user_avatar",
+                              "#{Rails.root}/spec/fixtures/files/image/image_test_3kb.gif"
+                  click_button "Sign up"
+                  expect(page).to have_content 'Welcome'
+                  click_on 'Profiles'
+                  expect(page).to have_content @general_user2.name
+                  expect(page).to have_css("img[src$='image_test_3kb.gif']")
+                end
+              end
+              context 'jpeg' do
+                it 'is available' do
+                  attach_file "user_avatar",
+                              "#{Rails.root}/spec/fixtures/files/image/image_test_3kb.jpeg"
+                  click_button "Sign up"
+                  expect(page).to have_content 'Welcome'
+                  click_on 'Profiles'
+                  expect(page).to have_content @general_user2.name
+                  expect(page).to have_css("img[src$='image_test_3kb.jpeg']")
+                end
+              end
+              context 'png' do
+                it 'is available' do
+                  attach_file "user_avatar",
+                              "#{Rails.root}/spec/fixtures/files/image/image_test_3kb.png"
+                  click_button "Sign up"
+                  expect(page).to have_content 'Welcome'
+                  click_on 'Profiles'
+                  expect(page).to have_content @general_user2.name
+                  expect(page).to have_css("img[src$='image_test_3kb.png']")
+                end
+              end
+              context 'svg' do
+                it 'is unavailable' do
+                  attach_file "user_avatar",
+                              "#{Rails.root}/spec/fixtures/files/image/image_test_3kb.svg"
+                  click_button "Sign up"
+                  expect(page).to have_content 'Sign up'
+                end
+              end
+              context 'bmp' do
+                it 'is unavailable' do
+                  attach_file "user_avatar",
+                              "#{Rails.root}/spec/fixtures/files/image/image_test_3kb.bmp"
+                  click_button "Sign up"
+                  expect(page).to have_content 'Sign up'
+                end
+              end
+            end
+            describe 'file size' do
+              context 'less then 5MB' do
+                it 'is available' do
+                  attach_file "user_avatar",
+                              "#{Rails.root}/spec/fixtures/files/image/image_test_5mb.jpeg"
+                  click_button "Sign up"
+                  expect(page).to have_content 'Welcome'
+                  click_on 'Profiles'
+                  expect(page).to have_content @general_user2.name
+                  expect(page).to have_css("img[src$='image_test_5mb.jpeg']")
+                end
+              end
+              context 'greater than 6MB' do
+                it 'is unavailable' do
+                  attach_file "user_avatar",
+                              "#{Rails.root}/spec/fixtures/files/image/image_test_6mb.jpeg"
+                  click_button "Sign up"
+                  expect(page).to have_content 'Sign up'
+                  expect(page).to have_content 'Avatar should be less than 5MB'
+                end
+              end
+            end
           end
         end
       end
