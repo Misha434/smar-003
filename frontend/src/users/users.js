62842f0da51f125b7310038fd3900b86790acd02
misha434 <missymouse434@gmail.com>
2021-08-22 13:52:08 +0900

    [Modify] Move JS code view files to frontend/
    ビュー上のJSコードをfrontend内のファイルに移動

--- frontend/src/users/users.js
+++ frontend/src/users/users.js
@@ -1,1 +1,13 @@
 import "./users.css";
+
+// Disabled Submit button without
+$(() => {
+  $("#signup--submit").prop("disabled", true);
+  $("#agreement").on(function confirmCheckBox() {
+    if ($(this).prop("checked") === false) {
+      $("#signup--submit").prop("disabled", true);
+    } else {
+      $("#signup--submit").prop("disabled", false);
+    }
+  });
+});
