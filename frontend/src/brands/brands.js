77e3a67132cd6e33267f6c0c0f978ff740a04571
misha434 <missymouse434@gmail.com>
2021-08-22 16:48:11 +0900

    [Modify] JS code view files brands#new, #edit
    ブランド画像 サイズバリデーション修正

--- frontend/src/brands/brands.js
+++ frontend/src/brands/brands.js
@@ -1,10 +1,1 @@
 import "./brands.css";
-
-// brand image_size varidation
-$("#brand_image").bind("change", () => {
-  const sizeInMegaBytes = this.files[0].size / 1024 / 1024;
-  if (sizeInMegaBytes > 5) {
-    alert("Maximum file size is 5MB. Please choose a smaller file.");
-    $("#brand_image").val("");
-  }
-});
