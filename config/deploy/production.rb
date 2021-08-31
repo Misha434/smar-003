06500d34f3956020d379217ac5ab2237ae18a71b
misha434 <missymouse434@gmail.com>
2021-09-01 01:45:36 +0900

    [CircleCI] Fix duplicate key

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,8 @@
 server '35.73.158.104', user: 'ec2-user', roles: %w{app db web}
 
 set :ssh_options, {
-  keys: keys: ['~/.ssh/id_rsa_f472fddc3fa078e857db2595610c1400'],
+  # keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  keys: ['~/.ssh/id_rsa_f472fddc3fa078e857db2595610c1400'],
   forward_agent: true,
   auth_methods: %w[publickey]
 }
