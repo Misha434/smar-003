46229a68a298e567850fb4401d61e78770de8fb1
misha434 <missymouse434@gmail.com>
2021-08-31 15:50:18 +0900

    [CircleCI] Add capistrano setting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,7 @@
 server '35.73.158.104', user: 'ec2-user', roles: %w{app db web}
+
+set :ssh_options, {
+  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  forward_agent: true,
+  auth_methods: %w[publickey]
+}
