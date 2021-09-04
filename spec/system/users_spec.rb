15a4a9567cf51b8084e5620d8683af251f9cff29
misha434 <missymouse434@gmail.com>
2021-09-04 19:56:18 +0900

    [RSpec] Add scroll script for fail case

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -423,6 +423,7 @@ RSpec.describe User, type: :system do
         fill_in 'パスワード', with: changed_password
         fill_in 'パスワード再入力', with: changed_password
         fill_in 'パスワード (変更前パスワード)', with: @general_user.password
+        execute_script("window.scrollTo(0, document.body.scrollHeight);")
         within('.actions') do
           click_on 'プロフィール編集'
         end
