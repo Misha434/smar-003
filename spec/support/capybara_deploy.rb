297d3befe561928608b45f9d7e78ad0c6b4e5dcf
misha434 <missymouse434@gmail.com>
2021-09-04 18:07:50 +0900

    [CircleCI] Add specific setting for CI

---
+++ spec/support/capybara_deploy.rb
@@ -0,0 +1,13 @@
+require "selenium/webdriver"
+Capybara.javascript_driver = :selenium_chrome_headless
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
+  end
+
+end
