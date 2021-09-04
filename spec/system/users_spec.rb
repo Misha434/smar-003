8681f53fd7d3a0d566e7574b34008c9bfa47da2f
misha434 <missymouse434@gmail.com>
2021-09-04 19:28:29 +0900

    [RSpec] Fix fail case for submit edit button

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -423,9 +423,7 @@ RSpec.describe User, type: :system do
         fill_in 'パスワード', with: changed_password
         fill_in 'パスワード再入力', with: changed_password
         fill_in 'パスワード (変更前パスワード)', with: @general_user.password
-        within('.actions') do
-          click_on 'プロフィール編集'
-        end
+        find('.post-button').click
         expect(page).to have_content 'Your account has been updated successfully.'
         within('header') do
           find(:css, 'button.dropdown-toggle').click
