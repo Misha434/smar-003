18c49b2474dc9208d94555ed02374e7e7738ea0a
misha434 <missymouse434@gmail.com>
2021-09-16 00:55:31 +0900

    Fix #255, Create star indicator helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -18,4 +18,22 @@ module ProductsHelper
     }
     sort_by[params[:q][:s]]
   end
+
+  def star_indicator(product)
+    if product.rate_average == 0
+      content_tag(:i, class: "fas fa-star mt-1")
+    else
+      product.rate_average.floor.times do
+        content_tag(:i, class: "fas fa-star active_star mt-1")
+      end
+      # 0.5 means base value for half-star should be indicated
+      if product.rate_average % 1 >= 0.5
+        content_tag(:i, class: "fas fa-star-half-alt active_star mt-1")
+      end
+      # 5 means max value of rating
+      (5 - product.rate_average.round).times do
+        content_tag(:i, class: "far fa-star no_active_star mt-1")
+      end
+    end
+  end
 end
