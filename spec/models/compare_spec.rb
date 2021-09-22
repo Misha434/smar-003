70709b24c523eeaf21e61b0b807531da9b8ff73b
misha434 <missymouse434@gmail.com>
2021-09-22 21:30:50 +0900

    Modify #258, Normal Test for bookmark feature
    製品比較機能 正常系テスト追加

--- spec/models/compare_spec.rb
+++ spec/models/compare_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Compare, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Data Validity' do
+    before do
+      @brand = FactoryBot.create(:brand)
+      @product = FactoryBot.create(:product)
+      @user = FactoryBot.create(:user)
+      @compare = Compare.new(user_id: 1, product_id: 1)
+    end
+    it 'is working' do
+      expect(@compare).to be_valid
+    end
+    it 'is not working without user_id' do
+      compare = Compare.new(user_id: '', product_id: 1)
+      expect(compare).to_not be_valid
+    end
+    it 'is not working without product_id' do
+      compare = Compare.new(user_id: 1, product_id: '')
+      expect(compare).to_not be_valid
+    end
+    it 'is not working duplicated data' do
+      @compare.save!
+      compare_duplicated = Compare.new(user_id: 1, product_id: 1)
+      expect(compare_duplicated).to_not be_valid
+    end
+    it 'is not working invalid type data (user_id)' do
+      compare_user_id_string = Compare.new(user_id: "あ", product_id: 1)
+      expect(compare_user_id_string).to_not be_valid
+    end
+    it 'is not working invalid type data (product_id)' do
+      compare_product_id_string = Compare.new(user_id: 1, product_id: "い")
+      expect(compare_product_id_string).to_not be_valid
+    end
+  end
 end
--- spec/system/compares_spec.rb
+++ spec/system/compares_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Compare, type: :system do
     before do
       @brand = FactoryBot.create(:brand)
       @product = FactoryBot.create(:product)
-      @product_2 = FactoryBot.create(:product, id: 2, name: "Phone-2")
-      @product_3 = FactoryBot.create(:product, id: 3, name: "Phone-3")
+      @product_2 = FactoryBot.create(:product, id: 2, name: "Phone-2", soc_antutu_score: 2000, battery_capacity: 2000)
+      @product_3 = FactoryBot.create(:product, id: 3, name: "Phone-3", soc_antutu_score: 3000, battery_capacity: 3000)
       @user = FactoryBot.create(:user)
       visit '/users/sign_in'
       fill_in "Email", with: @user.email
@@ -14,36 +14,101 @@ RSpec.describe Compare, type: :system do
       click_button "ログイン"
       visit '/products/1'
     end
-    describe 'Product Page' do
-      describe 'products/1'do
-        it 'has an available bookmark lists' do
+    describe 'products' do
+      describe '#show', js: true do
+        it 'has an available bookmark lists (0 Products)' do
           find('#bookmark--dropdown').click
-          click_on '登録一覧'
-          within('#compare-table') do
+          find('#bookmark--table--link').click
+          within('.modal-content') do
             expect(page).to have_content("お気に入り一覧")
             expect(page).to have_content("まだ登録されていません")
           end
         end
-        it 'has an available bookmark button' do
+        it 'has an available bookmark button (1 Product)' do
           find('#compare-disable').click
           find('#bookmark--dropdown').click
-          click_on '登録一覧'
-          within('#compare-table') do
+          find('#bookmark--table--link').click
+          within('.modal-content') do
             expect(page).to have_content("お気に入り一覧")
             expect(page).to have_content("Phone-1")
+            expect(find('.compare--battery1')).to have_content("1000")
+            expect(find('.compare--antutu1')).to have_content("1000")
+            expect(find('.compare--battery2')).to have_content("-")
+          end
+        end
+        it 'has an available bookmark button (2 Products)' do
+          find('#compare-disable').click
+          visit 'products/2'
+          find('#compare-disable').click
+          find('#bookmark--dropdown').click
+          find('#bookmark--table--link').click
+          within('.modal-content') do
+            expect(page).to have_content("お気に入り一覧")
+            expect(page).to have_content("Phone-1")
+            expect(page).to have_content("Phone-2")
+            expect(find('.compare--battery1')).to have_content("1000")
+            expect(find('.compare--antutu1')).to have_content("1000")
+            expect(find('.compare--battery2')).to have_content("2000")
+            expect(find('.compare--antutu2')).to have_content("2000")
+          end
+        end
+        it 'has not an available button to add bookmark (3 Products)' do
+          find('#compare-disable').click
+          visit 'products/2'
+          find('#compare-disable').click
+          visit 'products/3'
+          find('#compare-disable').click
+          expect(page).to have_content("2つ以上登録できません")
+          find('#bookmark--dropdown').click
+          find('#bookmark--table--link').click
+          within('.modal-content') do
+            expect(page).to have_content("お気に入り一覧")
+            expect(page).to have_content("Phone-1")
+            expect(page).to have_content("Phone-2")
+            expect(find('.compare--battery1')).to have_content("1000")
+            expect(find('.compare--antutu1')).to have_content("1000")
+            expect(find('.compare--battery2')).to have_content("2000")
+            expect(find('.compare--antutu2')).to have_content("2000")
+          end
+        end
+        it 'has an available button to remove bookmark (1 Product)' do
+          find('#compare-disable').click
+          find('#release-compare-disable').click
+          find('#bookmark--dropdown').click
+          find('#bookmark--table--link').click
+          within('.modal-content') do
+            expect(page).to have_content("お気に入り一覧")
+            expect(page).to have_content("まだ登録されていません")
           end
         end
       end
-      describe 'reset good button', js: true do
-        it 'is available' do
-          within '#review-1' do
-            click_link '0'
+      describe '#show, in modal', js: true do
+        it 'works delete and add bookmarks' do
+          find('#compare-disable').click
+          visit 'products/2'
+          find('#compare-disable').click
+          find('#bookmark--dropdown').click
+          find('#bookmark--table--link').click
+          within('.modal-content') do
+            expect(page).to have_content("お気に入り一覧")
+            expect(page).to have_content("Phone-2")
+            expect(find('.compare--battery2')).to have_content("2000")
+            expect(find('.compare--antutu2')).to have_content("2000")
+            find('.compare--delete2').click
           end
-          within '#review-1' do
-            click_link '1'
-          end
-          within 'span.each-like-countup' do
-            expect(page).to have_content("0")
+          page.driver.browser.switch_to.alert.accept
+          visit 'products/3'
+          find('#compare-disable').click
+          find('#bookmark--dropdown').click
+          find('#bookmark--table--link').click
+          within('.modal-content') do
+            expect(page).to have_content("お気に入り一覧")
+            expect(page).to have_content("Phone-1")
+            expect(page).to have_content("Phone-3")
+            expect(find('.compare--battery2')).to have_content("3000")
+            expect(find('.compare--antutu2')).to have_content("3000")
+            expect(find('.compare--battery1')).to have_content("1000")
+            expect(find('.compare--antutu1')).to have_content("1000")
           end
         end
       end
