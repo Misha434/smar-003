ddcbf70dfac0e772262bb08367137d1526ee0931
misha434 <missymouse434@gmail.com>
2021-09-22 21:59:14 +0900

    Modify #258, Change config for strong paramater
    製品比較機能 ストロングパラメータの設定変更

--- app/controllers/compares_controller.rb
+++ app/controllers/compares_controller.rb
@@ -1,22 +1,32 @@
 class ComparesController < ApplicationController
-  # before_action :compare_params, only: %i[destroy]
+  before_action :compare_params, only: %i[create destroy]
   before_action :authenticate_user!, only: %i[create destroy]
 
   def create
-    Compare.create(user_id: current_user.id, product_id: params[:id])
-    flash[:notice] = "Add Bookmark Successfully"
+    @compare = Compare.new(user_id: current_user.id, product_id: params[:id])
+    if @compare.save(compare_params)
+      flash[:notice] = "登録に成功しました"
+    else
+      @message = @compare.errors.full_messages.to_s.delete("^2つ以上登録できません")
+      @message ||= "登録に失敗しました"
+      flash[:danger] = @message
+    end
     redirect_to product_path
   end
-  
+
   def destroy
-    Compare.find_by(user_id: current_user.id, product_id: params[:id]).destroy
-    flash[:notice] = "Remove Bookmark Successfully"
+    @compare = Compare.find_by(user_id: current_user.id, product_id: params[:id])
+    if @compare.destroy
+      flash[:notice] = "登録解除しました"
+    else
+      flash[:danger] = "登録解除に失敗しました"
+    end
     redirect_to product_path
   end
 
   private
 
   def compare_params
-    @compare = Compare.find(params[:id])
+    params.permit(:user_id, :product_id)
   end
 end
